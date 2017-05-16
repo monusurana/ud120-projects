ada516f50180a1c3291835275be036811ac719b0
Monu Surana <monu.surana@gmail.com>
2017-05-15 22:29:53 -0700

    Used KNN,Adaboost & Random Forest Classifier for terrain classification

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+from time import time
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -31,14 +36,59 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+#KNN
+knn = KNeighborsClassifier(n_neighbors=20)
 
+t0 = time()
+knn.fit(features_train, labels_train)
+print "KNN linear training time:", round(time()-t0, 3), "s"
 
+t1 = time()
+pred = knn.predict(features_test)
+print "KNN linear prediction time:", round(time()-t1, 3), "s"
 
-
-
-
+accuracy = accuracy_score(pred, labels_test)
+print "KNN linear accuracy:", str(round(accuracy, 3)), "%"
 
 try:
-    prettyPicture(clf, features_test, labels_test)
+    prettyPicture(knn, features_test, labels_test, 'knn.jpg')
+except NameError:
+    pass
+
+# Adaboost
+ada = AdaBoostClassifier()
+
+t0 = time()
+ada.fit(features_train, labels_train)
+print "Adaboost linear training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = ada.predict(features_test)
+print "Adaboost linear prediction time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(pred, labels_test)
+print "Adaboost linear accuracy:", str(round(accuracy, 3)), "%"
+
+try:
+    prettyPicture(ada, features_test, labels_test, 'adaboost.jpg')
+except NameError:
+    pass
+
+# Random Forest Classifier
+rfc = RandomForestClassifier(min_samples_split=20)
+
+t0 = time()
+rfc.fit(features_train, labels_train)
+print "Random Forest linear training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = rfc.predict(features_test)
+print "Random Forest linear prediction time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(pred, labels_test)
+print "Random Forest linear accuracy:", str(round(accuracy, 3)), "%"
+
+try:
+    prettyPicture(rfc, features_test, labels_test, 'rfc.jpg')
 except NameError:
     pass
