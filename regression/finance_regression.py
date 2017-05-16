706c3711bf02001ccd4c8d7ddc8289e3c5ef270c
Monu Surana <monu.surana@gmail.com>
2017-05-15 22:45:11 -0700

    Used regression to predict financial data for Enron employees

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -15,8 +15,13 @@
 
 import sys
 import pickle
+
+import numpy as np
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import linear_model
+
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
 ### list the features you want to look at--first item in the 
@@ -29,21 +34,23 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
-
+test_color = "r"
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
 
+print "Slope:", reg.coef_
+print "Intercept:", reg.intercept_
+print "Mean squared error:", np.mean((reg.predict(feature_test) - target_test) ** 2)
 
-
-
-
-
+# Explained variance score: 1 is perfect prediction
+print('Test Score:', reg.score(feature_test, target_test))
+print('Training score:', reg.score(feature_train, target_train))
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -57,13 +64,17 @@ plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
 
-
-
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+print "Slope after training with test data:", reg.coef_
+
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
