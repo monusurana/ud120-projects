8c6089fa8a541f8b2d6f318daef1dcd3bc255e00
Monu Surana <monu.surana@gmail.com>
2017-06-25 20:59:19 -0700

    Implemented Cross Validation and used 30% of the data for testing

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,6 +12,8 @@
 
 import pickle
 import sys
+from sklearn import tree
+from sklearn import model_selection
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -26,7 +28,18 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+### it's all yours from here forward!
 
-### it's all yours from here forward!  
+clf = tree.DecisionTreeClassifier()
 
+clf.fit(features, labels)
+print "DT linear accuracy before CV:", clf.score(features, labels)
 
+features_train, features_test, labels_train, labels_test = model_selection.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print "DT linear accuracy after CV:", str(round(accuracy, 3)), "%"
